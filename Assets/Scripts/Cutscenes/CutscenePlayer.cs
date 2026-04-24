f5dca8b98a37ff2fd221a0e6a126cc1c97a08f0b
Alex874565 <alex.carpinisan@gmail.com>
2026-04-24 14:33:32 +0300

    merged

---
+++ Assets/Scripts/Cutscenes/CutscenePlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Unity.Cinemachine;
+using UnityEngine.Playables;
+using System.Collections;
+
+public class CutscenePlayer : MonoBehaviour
+{
+    public CinemachineCamera gameplayCam;
+    public CinemachineCamera cutsceneCam;
+    public PlayableDirector timeline;
+    public MonoBehaviour playerController;
+
+    public void Play()
+    {
+        StartCoroutine(PlayRoutine());
+    }
+
+    IEnumerator PlayRoutine()
+    {
+        playerController.enabled = false;
+
+        gameplayCam.Priority = 0;
+        cutsceneCam.Priority = 20;
+
+        timeline.Play();
+
+        yield return new WaitUntil(() => timeline.state != PlayState.Playing);
+
+        cutsceneCam.Priority = 0;
+        gameplayCam.Priority = 20;
+
+        playerController.enabled = true;
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,24 @@
 
 public class GameManager : MonoBehaviour
 {
+    
+    [SerializeField] private int runNo;
+    [Header("Player")]
     [SerializeField] private PlayerManager playerManager;
+    [SerializeField] private Transform playerTransform;
+
+    [Header("Cutscenes")]
+    [SerializeField] private CutscenePlayer introCutscene;
+    
+    private void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        playerManager = player.GetComponent<PlayerManager>();
+    }
     
     public void ResetGame()
     {
+        playerTransform.position = introCutscene.transform.position;
         playerManager.ResetAnxiety();
     }
 }
